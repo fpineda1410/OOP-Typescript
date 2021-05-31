d2d22e4a368df87c147fad1cf0fc94e443697c51
fpineda1410 <fpineda147@outlook.com>
2021-05-31 17:41:16 -0600

    First Typescript Practice

---
+++ src/main.js
@@ -0,0 +1,12 @@
+var BasicClass = /** @class */ (function () {
+    function BasicClass(name, lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+    BasicClass.prototype.completeName = function () {
+        return this.name + this.lastName;
+    };
+    return BasicClass;
+}());
+var myName = new BasicClass('el', 'bicho');
+console.log(myName.completeName());
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+
+
+class BasicClass {
+    name: string;
+    lastName: string;
+
+    constructor(name: string, lastName: string){
+        this.name = name;
+        this.lastName=lastName;
+    }
+
+    completeName():string{
+        return this.name+this.lastName;
+    }
+}
+
+
+let myName:BasicClass = new BasicClass ('el','bicho');
+
+console.log (myName.completeName());
+
